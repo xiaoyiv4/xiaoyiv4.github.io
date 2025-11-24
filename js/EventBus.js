23c10f734bfb81f7ac601bc93786ce800f0763a7
xiaoyiv4 <xiaoyiv4@users.noreply.github.com>
2025-11-24 12:49:15 +0000

    Deploy from dev branch - 5af3cefec58e55e9fc1c6e9916f514aeeb8c4134 5af3cefec58e55e9fc1c6e9916f514aeeb8c4134

---
+++ js/EventBus.js
@@ -0,0 +1,30 @@
+export class EventBus {
+    constructor() {
+        this.events = {};
+    }
+
+    on(event, callback) {
+        if (!this.events[event]) {
+            this.events[event] = [];
+        }
+        this.events[event].push(callback);
+    }
+
+    off(event, callback) {
+        if (!this.events[event]) return;
+
+        this.events[event] = this.events[event].filter(cb => cb !== callback);
+    }
+
+    emit(event, data) {
+        if (!this.events[event]) return;
+
+        this.events[event].forEach(callback => {
+            try {
+                callback(data);
+            } catch (error) {
+                console.error(`Error in event handler for ${event}:`, error);
+            }
+        });
+    }
+}
